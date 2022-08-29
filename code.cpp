493868dd725f083b3cbe1249cd3278244598f184
Nidhi-888 <56318217+Nidhi-888@users.noreply.github.com>
2022-08-29 06:26:39 -0700

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,16 @@
+//method_1
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int n = nums.size();
+        vector <int> ans;
+        int sum=0;
+        for (int i=0;i<n;i++)
+           
+        {sum+=nums[i];
+           ans.push_back(sum);
+        }
+        return ans;
+     
+    }
+};
